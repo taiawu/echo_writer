35d5139ae5a1c45c3bad90af67276599874114ae
taiawu <taiawu@gmail.com>
2021-05-17 13:51:26 -0700

    mostly working but without plots

--- R/app.R
+++ R/app.R
@@ -3,6 +3,7 @@ library(dsfworld)
 library(echowritr)
 library(glue)
 
+library(DT)
 library(stringdist)
 library(glue) # required for custom names within the selectors'
 library(shinyWidgets) # contains pickerInput
@@ -39,20 +40,18 @@ p("Step 1. Upload layouts for the mother and daughter plates.") %>% strong(),
                 uploadLayoutUI("daughter", 
                                "Upload daughter layout")[[1]], # upload panel
                 pickColUI("daughter_cols")[[1]], # the actual pickerInput
-                pickColUI("daughter_cols")[[2]], # should be the same
-                verbatimTextOutput("selection_external_daughter"),
                 
                 uploadLayoutUI("mother", 
                                "Upload mother layout")[[1]],
                 pickColUI("mother_cols")[[1]], #[[1]],  # the actual pickerInput
-                pickColUI("mother_cols")[[2]], # should be the same
-                #tags$hr(style="border-color: #d9d9d9;"),
+               
                 numericInput(
                   "echo_vol", 
                   "Step 2. Set the droplet volume (nL) for the Echo instrument.",
                   value = 25, 
                   min = 0, 
                   max = 100),
+
                 pickerInput(
                   "repair_methods",
                   label = "Step 3.  Resolve any issues in the requested transfers", # user can change the label
@@ -68,16 +67,20 @@ p("Step 1. Upload layouts for the mother and daughter plates.") %>% strong(),
                 #tags$hr(style="border-color: #d9d9d9;"),
 textInput("save_name", "Step 4. Enter names for the downloaded plots and files.", value = Sys.Date()),
 textAreaInput("notes", "", rows = 2, value = "Add any notes here"),
-                actionButton("go", "Calculate Echo transfers", class = "btn-block")
+               
+# numericInput(
+#   "max_vol", 
+#   "Notify if a single mother well is depleted by more than uL to draw from a single well.",
+#   value = 35, 
+#   min = 0, 
+#   max = 100),
+actionButton("go", "Calculate Echo transfers", class = "btn-block")
               ),
               mainPanel(
                 # Output: Tabset w/ instructions, plots, and user instructions ----
                 tabsetPanel(type = "tabs",
                             tabPanel("Echo instructions", 
-                                     p("tables will go here"),
-                                     p("Transfers") %>% strong(),
-                                     verbatimTextOutput("transfer_print"),
-                                     tableOutput("transfer_table")),
+                                     dataTableOutput("transfer_table"), style = "overflow-x: scroll;overflow-y: scroll;"),
                             tabPanel("Plots", p("plots will go here")),
                             tabPanel("How to use this app", p("instructions will go here"))
                 )
@@ -115,18 +118,9 @@ server <-
           sapply( . , function(x) 
           {str_split(x, pattern = "<>")[[1]][2]}))
     
-    daughter <- 
-      reactive(
-        daughter_raw() %>%
-          standardize_layout( . ,
-                              "daughter",
-                              .well_col = daughter_cols()[[1]],
-                              .compound_col = daughter_cols()[[2]],
-                              .concentration_col = daughter_cols()[[3]],
-                              .volume_col = daughter_cols()[[4]]))
     
     output$table_external_daughter <- 
-      renderTable(head(daughter()))
+      renderDataTable(head(daughter()))
     
     output$selection_external_daughter <- 
       renderPrint(picker_list_daughter())
@@ -156,74 +150,40 @@ server <-
           {str_split(x, pattern = "<>")[[1]][2]}))
     
     
-    mother <- 
-      reactive(
-        mother_raw() %>%
-          standardize_layout( . ,
-                              "mother",
-                              .well_col = mother_cols()[[1]],
-                              .compound_col = mother_cols()[[2]],
-                              .concentration_col = mother_cols()[[3]]))
-    
     output$selection_external_mother <- 
       renderPrint(picker_list_mother())
     
     output$table_external_mother <- 
       renderTable({head(mother())})
-    
-    ###### pop up the error messages automatically, and on their own...
-    ####### how to repair issues
-    layouts <- reactive({
-      # req(mother()) # cancel until mother exists
-      # req(daughter()) # cancel until daughter exists
-      #     #input$accept_and_run # depend on user saying "accept these fixes and run with it"
-      
-      try_catch_popup(
-        execute_this = rlang::abort(all(daughter()$compound %in% mother()$compound)),
-        error_title = "Daughter plate has compounds not present in the mother plate",
-        error_subtitle = 
-          glue::glue("Edit your layout, or see the 'Repair methods' 
-                 drop-down menu for ways to proceed anyway. 
-                 \nMissing compounds: {
-                 glue_collapse(unique(
-                 daughter()$compound[!daughter()$compound %in% mother()$compound]), 
-                 sep = ', ')}"))
-      
-      try_catch_popup(
-        execute_this = rlang::abort(get_varied_errors(mother())$outcome),
-        error_title = "Single compound present in mother at multiple concentrations",
-        error_subtitle = 
-          glue::glue("This isn't supported at this time. 
-                 Edit your mother layout, or see the 'Repair methods' 
-                 drop-down menu for ways to proceed anyway. 
-                \nThis issue was found in compounds:
-                {glue_collapse(get_varied_errors(mother())$compounds, sep = ', ')}"))
-      
-      try_catch_popup(
-        execute_this = rlang::abort(get_conc_errors(daughter(), mother())$outcome),
-        error_title = "Daughter plate has un-achievably high concentrations",
-        error_subtitle = 
-          glue::glue("Edit your layout, or see the 'Repair methods' 
-                        drop-down menu for ways to proceed anyway. 
-                      \nThis issue was found in wells: 
-                      {glue_collapse(get_conc_errors(daughter(), 
-                      mother())$wells, sep = ', ')},
-                      \nCorresponding to compounds:
-                      {glue_collapse(get_conc_errors(daughter(), 
-                       mother())$compounds, sep = ', ')}"))
-      
-      # try_catch_popup( execute_this = repair_layout(mother(),
-      #                                               daughter(),
-      #                                               if_missing = "drop", # input$repair_choices$something something -- need f() for pickerInput to these values conversion
-      #                                               if_varied = "keep_max", # input$repair_choices$something something -- need f() for pickerInput to these values conversion
-      #                                               if_impossible = "scale_down"), # input$repair_choices$something something -- need f() for pickerInput to these values conversion
-      #                  error_title = "Couldn't clean and repair the layouts",
-      #                  error_subtitle = glue::glue("Please check your layouts and try again")
-      # )
-      
-    })
+  
     
     observeEvent(input$go, {
+      try_catch_popup(
+        daughter <- 
+          reactive(
+            daughter_raw() %>%
+              standardize_layout( . ,
+                                  "daughter",
+                                  .well_col = daughter_cols()[[1]],
+                                  .compound_col = daughter_cols()[[2]],
+                                  .concentration_col = daughter_cols()[[3]],
+                                  .volume_col = daughter_cols()[[4]])) ,
+        error_title = "daughter standard error"
+        
+      )
+      
+      try_catch_popup(
+        mother <- 
+          reactive(
+            mother_raw() %>%
+              standardize_layout( . ,
+                                  "mother",
+                                  .well_col = mother_cols()[[1]],
+                                  .compound_col = mother_cols()[[2]],
+                                  .concentration_col = mother_cols()[[3]])),
+        error_title = "mother standard error"
+      )
+
       repairs <- 
         reactive(
           get_repairs(
@@ -231,17 +191,20 @@ server <-
             translate_repairs) 
         )
       
-      try_catch_popup(
-        execute_this = rlang::abort(all(daughter()$compound %in% mother()$compound)),
-        error_title = "Daughter plate has compounds not present in the mother plate",
-        error_subtitle = 
-          glue::glue("Edit your layout, or see the 'Repair methods' 
+      if (repairs()[["repair_missing"]] == "stop") {
+        try_catch_popup(
+          execute_this = rlang::abort(all(daughter()$compound %in% mother()$compound)),
+          error_title = "Daughter plate has compounds not present in the mother plate",
+          error_subtitle = 
+            glue::glue("Edit your layout, or see the 'Repair methods' 
                  drop-down menu for ways to proceed anyway. 
                  \nMissing compounds: {
                  glue_collapse(unique(
                  daughter()$compound[!daughter()$compound %in% mother()$compound]), 
                  sep = ', ')}"))
-      
+      }
+
+      if (repairs()[["repair_varied"]] == "stop") {
       try_catch_popup(
         execute_this = rlang::abort(get_varied_errors(mother())$outcome),
         error_title = "Single compound present in mother at multiple concentrations",
@@ -251,7 +214,9 @@ server <-
                  drop-down menu for ways to proceed anyway. 
                 \nThis issue was found in compounds:
                 {glue_collapse(get_varied_errors(mother())$compounds, sep = ', ')}"))
+      }
       
+      if (repairs()[["repair_conc"]] == "stop") {
       try_catch_popup(
         execute_this = rlang::abort(get_conc_errors(daughter(), mother())$outcome),
         error_title = "Daughter plate has un-achievably high concentrations",
@@ -264,7 +229,9 @@ server <-
                       \nCorresponding to compounds:
                       {glue_collapse(get_conc_errors(daughter(), 
                        mother())$compounds, sep = ', ')}"))
+      }
       
+      try_catch_popup(
       layouts <- 
         reactive(
         repair_layout(
@@ -272,50 +239,40 @@ server <-
           daughter(),
           if_missing = repairs()[["repair_missing"]], 
           if_varied = repairs()[["repair_varied"]], 
-          if_impossible = repairs()[["repair_conc"]]))
+          if_impossible = repairs()[["repair_conc"]])),
+      error_title = "issue with making layout"
+      )
       
+      try_catch_popup(
       transfers <- 
         reactive(
           calculate_transfers(
             layouts()$daughter, 
             layouts()$mother, 
             input$echo_vol) 
-        )
+        ),
+      error_title = "issue with getting transfers"
+      )
         
+      
+      try_catch_popup(
       depletion <-
         reactive(
           monitor_source_depletion(
             transfers(), 
-            35) ### THIS SHOULD BE USER-SET!! Or at least, have the option to be
-          )
+            35
+            ) ### THIS SHOULD BE USER-SET!! Or at least, have the option to be .. input$max_vol
+          ),
+      error_title = "depletions!"
+      )
       
       output$transfer_table <-
-        renderTable({
+        renderDataTable({
           transfers()
         })
         
     })
-    
-    
-    # ##### what's going on here? 
-    # # how do you wait to execute until a reactive expression exits?
-    # transfers <- reactive({
-    #   # req( layouts() ) # cancel if layout() doesn't exist yet
-    #   calculate_transfers(layouts()$daughter, layouts()$mother, input$echo_vol)
-    # })
-    # 
-    # depletion <- reactive({
-    #   # req( is.reactive(transfers() )) # what is transfers fails--should we do a if(success) or return NULL for transfers calc?
-    #   monitor_source_depletion(transfers(), 35)
-    # })
-    
-    # output$transfer_print <-
-    #   renderPrint({
-    #     req(is.reactive(transfers()))
-    #     
-    #     print(transfers())
-    #     transfers()
-    #   })
+  
   }
 
 shinyApp(ui = ui, server = server)
